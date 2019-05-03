e00a59b0a12633241b0f5d3078ea8782cdc31407
ohkaiju <lcmte.ju@gmail.com>
2019-05-03 11:18:08 +0200

    Update SEO.js

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -66,18 +66,26 @@ const SEO = ({ albumData }, isa) => {
   return (
     <>
       <Helmet>
-        {/* General tags */}
+        {/* General */}
         <html lang={siteLanguage} />
         <title>{title}</title>
         <meta name="description" content={description} />
         <meta name="image" content={image} />
         <link rel="canonical" href={url} />
 
-        {/* OpenGraph tags */}
+        {/* OpenGraph */}
         <meta property="og:url" content={url} />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
         <meta property="og:image" content={image} />
+
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={image} />
+
+
         <script type="application/ld+json">{JSON.stringify(schema)}</script>
       </Helmet>
     </>
@@ -86,7 +94,7 @@ const SEO = ({ albumData }, isa) => {
 
 SEO.defaultProps = {
   isAlbum: false,
-  albumData: {},
+  albumData: {}
 }
 
 export default SEO
